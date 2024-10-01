b94b22474dda94e9dc20982e701012684829f798
Rituraj pandey <117599621+rituraj981@users.noreply.github.com>
2024-10-01 08:48:21 +0000

    structured files

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ const recognition = new speechRecognition();
 recognition.onstart = function(){
   console.log("you can speek now");  
 }
+
 recognition.onresult = function(event){
   var text = event.results[0][0].transcript;
   console.log(text);
   document.getElementById("result").innerHTML = text;
 }
+
 function copyDivToClipboard() {
   var range = document.createRange();
   range.selectNode(document.getElementById("result"));
